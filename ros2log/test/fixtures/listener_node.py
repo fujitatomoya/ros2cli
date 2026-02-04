beae1292c22247fad42ad008ddb9c83cd3c38d82
Decwest <fumiyaonishi1016@gmail.com>
2026-02-04 22:47:44 +0900

    bug: fix test
    Signed-off-by: Decwest <fumiyaonishi1016@gmail.com>

--- ros2log/test/fixtures/listener_node.py
+++ ros2log/test/fixtures/listener_node.py
@@ -24,7 +24,7 @@ class ListenerNode(Node):
     """Test node that subscribes to messages and generates logs."""
 
     def __init__(self):
-        super().__init__('listener')
+        super().__init__('listener', enable_logger_service=True)
         self.subscription = self.create_subscription(
             String,
             'chatter',
